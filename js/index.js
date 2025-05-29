5cd584305cadc9e39fef14e8b1ec1867d2b0c4ae
Nazar Prytula <prytula3103@gmail.com>
2025-05-29 18:14:09 +0200

    1

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// 1
+let result = 5 + 5 + '5';
+console.log(result);
+console.log(typeof result);
+
+// 2
+let email = 'example@email.com';
+let containsAt = email.includes('@');
+let emailLength = email.length;
+console.log("Містить '@':", containsAt);
+console.log("Кількість символів у email:", emailLength);
+
+// 3
+let word1 = 'My';
+let word2 = 'name';
+let word3 = 'is';
+let fullName = `${word1} ${word2} ${word3}`;
+fullName += ' Viktor';
+console.log("Повне ім’я:", fullName);
+
+// 4
+et userName = 'Олександр';
+let payment = 300;
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
